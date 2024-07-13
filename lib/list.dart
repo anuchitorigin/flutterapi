d2dc267222d3b750b4a471d8bbed3712004c01f0
Anuchit <anuchit.b@origin55.com>
2024-07-14 00:06:12 +0700

    fix ListView

--- lib/list.dart
+++ lib/list.dart
@@ -11,53 +11,90 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  String title = '';
-  String key = '';
-  dynamic result = [];
+  var loading = 0;
+  var title = '';
+  var key = '';
+  List result = [
+    {'INDEXID': '1', 'SITEID': 's1'},
+    {'INDEXID': '2', 'SITEID': 's2'},
+    {'INDEXID': '3', 'SITEID': 's3'},
+    {'INDEXID': '4', 'SITEID': 's4'},
+    {'INDEXID': '5', 'SITEID': 's5'},
+    {'INDEXID': '6', 'SITEID': 's6'},
+    {'INDEXID': '7', 'SITEID': 's7'},
+    {'INDEXID': '8', 'SITEID': 's8'},
+    {'INDEXID': '9', 'SITEID': 's9'},
+    {'INDEXID': '10', 'SITEID': 's10'},
+  ];
 
   Future<dynamic> postSQL(String sql) async {
-    final response = await http.post(
-      Uri.parse('http://localhost:55016/datasnap/rest/TServerMethods2/query/sql'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'sql': sql,
-      }),
+    final response = await http.get(
+      // Uri.parse('http://localhost:55016/datasnap/rest/TServerMethods2/query/sql'),
+      Uri.parse('https://jsonplaceholder.typicode.com/posts/1'),
+      // headers: <String, String>{
+      //   'Content-Type': 'application/json; charset=UTF-8',
+      // },
+      // body: jsonEncode(<String, String>{
+      //   'sql': sql,
+      // }),
     );
+    final res = jsonDecode(response.body);
+    print(res);
 
     if ([200, 201].contains(response.statusCode)) {
-      // If the server did return a 201 CREATED response,
-      // then parse the JSON.
       setState(() {
-        final res = jsonDecode(response.body);
-        print(res['result'][0]['result']);
-        result = [
-          {
-            'INDEXID': 'INDEXID', 
-            'SITEID': 'SITEID'
-          },
-        ];
+        loading = 2;
+        // print(res['result'][0]['result']);
+        // result = [
+        //   {'INDEXID': 'INDEXID', 'SITEID': 'SITEID'},
+        // ];
         // result = res[0]['result'];
       });
     } else {
-      // If the server did not return a 201 CREATED response,
-      // then throw an exception.
-      // throw Exception('Failed to post SQL.');
       setState(() {
+        loading = 2;
         result = [];
       });
     }
   }
 
+  Widget itemList() {
+    if (loading < 2) {
+      return const CircularProgressIndicator();
+    } else {
+      return result.isEmpty
+          ? const Text('No data found')
+          : SizedBox(
+              height: 300.0,
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: result.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(result[index]['INDEXID']),
+                    subtitle: Text(result[index]['SITEID']),
+                  );
+                },
+              ),
+            );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-    final arg = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    final arg =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
 
     title = arg['title'];
     key = arg['key'];
 
+    if (loading == 0) {
+      postSQL('SELECT TOP 10 * FROM sites;');
+      loading = 1;
+    }
     // switch (key) {
     //   case 'SITEID':
     //     postSQL('SELECT TOP 10 * FROM sites;');
@@ -76,22 +113,7 @@ class _ListPageState extends State<ListPage> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             const SizedBox(height: 30),
-            result.isEmpty
-                ? const CircularProgressIndicator()
-                : ListView.builder(
-                  // Let the ListView know how many items it needs to build.
-                  // itemCount: result.length,
-                  // Provide a builder function. This is where the magic happens.
-                  // Convert each item into a widget based on the type of item it is.
-                  itemBuilder: (context, index) {
-                    final item = result[index];
-
-                    return ListTile(
-                      title: Text(item['INDEXID']),
-                      subtitle: Text(item[key]),
-                    );
-                  },
-                ),
+            Center(child: itemList()),
             const SizedBox(height: 30),
             ElevatedButton(
               style: style,
